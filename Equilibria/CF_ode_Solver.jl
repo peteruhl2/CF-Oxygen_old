adfb475d05ea8f91443a3f58e7df13d09efb99ad
peteruhl2 <59039799+peteruhl2@users.noreply.github.com>
2021-02-03 20:31:36 -0800

    Feb 3

--- Equilibria/CF_ode_Solver.jl
+++ Equilibria/CF_ode_Solver.jl
@@ -25,8 +25,8 @@ function cf_ode!(yp,y,p,t)
 end
 
 # ode parameters
-Ec = 16.0049; Ac = 0.0139; nc = 1.0
-rf = 14.0436; d = 0.7016; ϵ = 0.0; μ = 1.0273;
+Ec = 16.95036; Ac = 0.0139; nc = 1.0
+rf = 15.55036; d = 0.6016; ϵ = 0.0; μ = 1.0273;
 k = 10000
 η = 0.8176/k
 λ = 0.22
@@ -34,8 +34,8 @@ k = 10000
 p = [Ec,Ac,nc,rf,d,ϵ,μ,η,k]
 
 # y0 = [0.8283,0.0165,0.1388] # best fitting ode parameters
-y0 = [0.8789,0.0165,0.1388]
-tspan = (0.0,10000.0)
+y0 = [0.8789,0.00000565,0.1388]
+tspan = (0.0,1000.0)
 prob = ODEProblem(cf_ode!,y0,tspan,p)
 sol = solve(prob)
 
@@ -43,15 +43,16 @@ c = sol[1,:]
 f = sol[2,:]
 
 ### stability conditions
-C_invade = Ec*λ - rf*λ  - Ac*rf*μ
-
-B = Ac*d*k*η + Ec*λ
-D = rf*(d*λ + Ac*λ*μ - Ec*λ)
-F_invade = B*(rf-d) + D
+x1 = Ec*λ - rf*λ - Ac*rf*μ
+x2 = (rf - d)*(Ac*d*k*η + Ec*λ) + rf*d*λ + rf*Ec*λ*μ - rf*Ec*λ
+# xx2 = rf*Ac*d*k*η - Ac*(d^2)*k*η - d*Ec*λ + rf*d*λ + rf*Ac*λ*μ
 
 
 p = plot(sol.t[c.>0],c[c.>0], label = "C ODE", lw = 2, yaxis=:log)
 p = plot!(sol.t[f.>0],f[f.>0], label = "F ODE", lw = 2, legend=:left, xlabel = "Time (days)", ylabel = "Population Density",yaxis=:log)
 display(p)
 
-println("C_invade is ", C_invade," F_invade is ", F_invade)
+# println("C_invade is ", C_invade," F_invade is ", F_invade)
+println(x1)
+println(x2)
+println()
